35801be99533f4f4aae723f58990e7c108d12fc0
Henrique <69041032+ApiLogik@users.noreply.github.com>
2021-01-21 14:09:31 -0300

    Add files via upload

---
+++ js/custom-lib.js
@@ -0,0 +1,138 @@
+//Vanilla JS slideUp, slideDown and slideToggle
+
+
+    /* SLIDE UP */
+    const slideUp = (target, duration=500) => {
+
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.boxSizing = 'border-box';
+        target.style.height = target.offsetHeight + 'px';
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout( () => {
+              target.style.display = 'none';
+              target.style.removeProperty('height');
+              target.style.removeProperty('padding-top');
+              target.style.removeProperty('padding-bottom');
+              target.style.removeProperty('margin-top');
+              target.style.removeProperty('margin-bottom');
+              target.style.removeProperty('overflow');
+              target.style.removeProperty('transition-duration');
+              target.style.removeProperty('transition-property');
+              //alert("!");
+        }, duration);
+    }
+
+    /* SLIDE DOWN */
+    const slideDown = (target, duration=500) => {
+
+        target.style.removeProperty('display');
+        let display = window.getComputedStyle(target).display;
+        if (display === 'none') display = 'block';
+        target.style.display = display;
+        let height = target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.boxSizing = 'border-box';
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        window.setTimeout( () => {
+          target.style.removeProperty('height');
+          target.style.removeProperty('overflow');
+          target.style.removeProperty('transition-duration');
+          target.style.removeProperty('transition-property');
+        }, duration);
+    }
+
+    /* TOGGLE */
+    const slideToggle = (target, duration = 500) => {
+        if (window.getComputedStyle(target).display === 'none') {
+          return slideDown(target, duration);
+        } else {
+          return slideUp(target, duration);
+        }
+    }
+
+
+
+// Specify Element and Duration (milliseconds)
+/*
+document.querySelector("#button-slide").addEventListener('click', () => {
+	slideToggle(document.getElementById("slide"), 1000);
+	slideDown(document.getElementById("slide"), 1000);
+	slideUp(document.getElementById("slide"), 1000);
+})
+*/
+
+//------------------------------------------------------------------------------
+
+// Vanilla JavaScript Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+
+(function() {
+	scrollTo();
+})();
+
+function scrollTo() {
+	const links = document.querySelectorAll('.scroll');
+	links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+	e.preventDefault();
+	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+	const targetAnchor = document.querySelector(targetID);
+	if (!targetAnchor) return;
+	const originalTop = distanceToTop(targetAnchor);
+	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+	const checkIfDone = setInterval(function() {
+		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+		if (distanceToTop(targetAnchor) === 0 || atBottom) {
+			targetAnchor.tabIndex = '-1';
+			targetAnchor.focus();
+			window.history.pushState('', '', targetID);
+			clearInterval(checkIfDone);
+		}
+	}, 100);
+}
+
+//--------------------------------------------------------------------------------
+// functions: show(), hide() and toggle()
+const show = element => element.style.display = 'block';
+
+const hide = element => element.style.display = 'none';
+
+const toggle = element => {
+	if (window.getComputedStyle(element).display === 'block') {
+		hide(element);
+		return;
+	} else show(element);
+}
+
+//Slide side-to-side menu
+const sideIn = (obj, startPos, endPos, speed) => {
+	obj.animate([{width: startPos},{width: endPos}], speed);
+	obj.style.width = endPos;
+}
+
+const sideOut = (obj, startPos, endPos, speed) => {
+	obj.animate([{width: startPos},{width: endPos}], speed);
+	obj.style.width = endPos;
+}
